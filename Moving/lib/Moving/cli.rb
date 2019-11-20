3ebd95e31935591c1fa85d5a3d5df8d8e0cf7966
Janay Bailey <tjanay87@gmail.com>
2019-11-20 02:02:21 +0000

    refactoring

--- Moving/lib/Moving/cli.rb
+++ Moving/lib/Moving/cli.rb
@@ -11,29 +11,32 @@ class Moving::CLI
   end 
   
   def vehicle_selection()
-     prompt = nil 
     
-    Room.all.each.with_index(1) do  |r,index|
-      puts "#{index}. #{r.name}"
+    Truck.all.each.with_index(1) do  |r,index|
+      puts "#{index}. #{r.room.name}"
     end
     
     puts "How many rooms are you moving?"
     prompt = gets.chomp.to_i 
-    
-  if prompt -1 > 0 && prompt -1 < room.length 
-    puts  "Please check your entry and try again!"
+    @truck = Truck.all[prompt -1]
+  
+    if prompt > 0 && prompt < Room.all.length 
+    puts "Wow. You should try #{@truck.name}."
 
   else 
-         puts "Wow. You should try #{truck.name}."
+    puts  "Please check your entry and try again!"
+         
   end 
 end 
 
   def fuel_tank()
-     puts "Would you like to know how much gas to use?" 
+     puts "Would you like to know how much gas to use? please type y/n" 
+    prompt = gets.chomp.downcase
     
-    @gas = Gas.all[user -1]
-     while (not reply)
-      reply = gets.chomp.downcase
-    end 
+    if prompt == 'y' 
+      puts "You will use #{@truck.gas.amount}."
+    else  
+      puts "Thank you for visiting our page!"
+    end
     end
  end
